8abf36111ec40e50c4b27469a6f7fb456d0d18e8
Fatma <chandoulfatma20598@gmail.com>
2023-01-14 12:17:08 +0100

    first commit

---
+++ BlazerApp_Security/Program.cs
@@ -0,0 +1,47 @@
+using BlazerApp_Security.Data;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+var connectionString = builder.Configuration.GetConnectionString("BlazerApp_SecurityContextConnection") ?? throw new InvalidOperationException("Connection string 'BlazerApp_SecurityContextConnection' not found.");
+
+builder.Services.AddAuthentication("Identity.Application").AddCookie();
+
+builder.Services.AddDbContext<BlazerApp_SecurityContext>(options =>
+    options.UseSqlServer(connectionString));
+
+builder.Services.AddDefaultIdentity<IdentityUser>(options => options.SignIn.RequireConfirmedAccount = true)
+    .AddEntityFrameworkStores<BlazerApp_SecurityContext>();
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddSingleton<WeatherForecastService>();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+    app.UseHsts();
+}
+
+
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+app.UseAuthentication();
+app.UseAuthorization();
+
+
+app.Run();
